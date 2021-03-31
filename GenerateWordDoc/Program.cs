d0576641cae2e64e75226825c10c995e8320e63b
Daniel Burnley <dan@danops.uk>
2021-03-31 17:09:29 +0100

    Make it nicer to add tables

--- GenerateWordDoc/Program.cs
+++ GenerateWordDoc/Program.cs
@@ -8,6 +8,18 @@ namespace GenerateWordDoc
 {
     class Program
     {
+        private class BuilderTableCell
+        {
+            public string Text = "";
+            public BuilderTableCellOptions Options;
+        }
+
+        private class BuilderTableCellOptions
+        {
+            public bool Bold;
+            public bool MergeAbove;
+        }
+
         private class DocumentBuilder
         {
             private readonly WordprocessingDocument _document;
@@ -22,7 +34,7 @@ namespace GenerateWordDoc
                 Body = _mainPart.Document.Body;
             }
 
-            public void AddParagraph(string text)
+            public void AddParagraphToDocument(string text)
             {
                 var run = new Run(new Text(text));
                 var runProperties = new RunProperties();
@@ -33,15 +45,63 @@ namespace GenerateWordDoc
                 Body.AppendChild(para);
             }
 
-            public void AddTable(List<List<string>> data)
+            public void AddTable(List<List<BuilderTableCell>> data)
             {
                 var table = new Table();
+
+                var tableProperties = new TableProperties
+                {
+                    TableBorders = new TableBorders
+                    {
+                        TopBorder = new TopBorder
+                            {Color = "000000", Val = BorderValues.Single, Size = 8, Space = 0},
+                        RightBorder = new RightBorder
+                            {Color = "000000", Val = BorderValues.Single, Size = 8, Space = 0},
+                        BottomBorder = new BottomBorder
+                            {Color = "000000", Val = BorderValues.Single, Size = 8, Space = 0},
+                        LeftBorder = new LeftBorder
+                            {Color = "000000", Val = BorderValues.Single, Size = 8, Space = 0},
+                        InsideVerticalBorder = new InsideVerticalBorder
+                            {Color = "000000", Val = BorderValues.Single, Size = 8, Space = 0},
+                        InsideHorizontalBorder = new InsideHorizontalBorder
+                            {Color = "000000", Val = BorderValues.Single, Size = 8, Space = 0},
+                    }
+                };
+
+                table.AppendChild(tableProperties);
+
                 data.ForEach(dataRow =>
                 {
                     var tableRow = new TableRow();
                     dataRow.ForEach(dataCell =>
                     {
-                        tableRow.AppendChild(new TableCell(new Paragraph(new Run(new Text(dataCell)))));
+                        var tableCell = new TableCell();
+                        var tableCellProperties = new TableCellProperties
+                        {
+                            VerticalMerge = new VerticalMerge
+                            {
+                                Val = MergedCellValues.Restart
+                            }
+                        };
+
+                        if (dataCell.Options != null && dataCell.Options.MergeAbove)
+                        {
+                            tableCellProperties.VerticalMerge.Val = MergedCellValues.Continue;
+                        }
+
+                        tableCell.TableCellProperties = tableCellProperties;
+                        
+                        var paragraph = new Paragraph();
+                        var run = new Run(new Text(dataCell.Text));
+
+                        if (dataCell.Options != null && dataCell.Options.Bold)
+                        {
+                            run.RunProperties = new RunProperties(new Bold());
+                        }
+
+                        paragraph.AppendChild(run);
+                        tableCell.AppendChild(paragraph);
+                        tableRow.AppendChild(tableCell);
                     });
                     table.AppendChild(tableRow);
                 });
@@ -54,84 +114,46 @@ namespace GenerateWordDoc
                 _document.Save();
                 _document.Close();
             }
+
+            public void AddLineBreak()
+            {
+                AddParagraphToDocument("");
+            }
         }
 
         static void Main(string[] args)
         {
             var builder = new DocumentBuilder(args[0]);
 
-            var table = new Table();
-            var tableProperties = new TableProperties
+            var rows = new List<List<BuilderTableCell>>
             {
-                TableBorders = new TableBorders
+                new List<BuilderTableCell>
                 {
-                    TopBorder = new TopBorder
-                        {Color = "000000", Val = BorderValues.Single, Size = 8, Space = 0},
-                    RightBorder = new RightBorder
-                        {Color = "000000", Val = BorderValues.Single, Size = 8, Space = 0},
-                    BottomBorder = new BottomBorder
-                        {Color = "000000", Val = BorderValues.Single, Size = 8, Space = 0},
-                    LeftBorder = new LeftBorder
-                        {Color = "000000", Val = BorderValues.Single, Size = 8, Space = 0},
-                    InsideVerticalBorder = new InsideVerticalBorder
-                        {Color = "000000", Val = BorderValues.Single, Size = 8, Space = 0},
-                    InsideHorizontalBorder = new InsideHorizontalBorder
-                        {Color = "000000", Val = BorderValues.Single, Size = 8, Space = 0},
+                    new BuilderTableCell {Text = "Recommendation", Options = new BuilderTableCellOptions {Bold = true}},
+                    new BuilderTableCell {Text = "A bunch of example text etc etc with some more to pad the text out"},
+                    new BuilderTableCell {Text = "Date:", Options = new BuilderTableCellOptions {Bold = true}},
+                    new BuilderTableCell {Text = "01/09/2020"}
+                },
+                new List<BuilderTableCell>
+                {
+                    new BuilderTableCell {Options = new BuilderTableCellOptions {MergeAbove = true}},
+                    new BuilderTableCell {Options = new BuilderTableCellOptions {MergeAbove = true}},
+                    new BuilderTableCell {Text = "Author:", Options = new BuilderTableCellOptions {Bold = true}},
+                    new BuilderTableCell {Text = "Meow Meowington"}
+                },
+                new List<BuilderTableCell>
+                {
+                    new BuilderTableCell
+                        {Text = "Is AO Required?", Options = new BuilderTableCellOptions {Bold = true}},
+                    new BuilderTableCell {Text = "No"},
+                    new BuilderTableCell {Text = "Cleared by:", Options = new BuilderTableCellOptions {Bold = true}},
+                    new BuilderTableCell {Text = "Barks Barkington"}
                 }
             };
 
-            table.AppendChild(tableProperties);
-
-            var tableRow = new TableRow();
-
-            tableRow.AppendChild(
-                new TableCell(new Paragraph(new Run(new Text("Recommendation:"))
-                    {RunProperties = new RunProperties(new Bold())}))
-                {
-                    TableCellProperties = new TableCellProperties
-                    {
-                        VerticalMerge = new VerticalMerge {Val = MergedCellValues.Restart}
-                    }
-                });
-            tableRow.AppendChild(new TableCell(new Paragraph(new Run(new Text(
-                "A bunch of example text etc etc etc and some more text to make the box quite large and stuff"))))
-            {
-                TableCellProperties = new TableCellProperties
-                    {VerticalMerge = new VerticalMerge {Val = MergedCellValues.Restart}}
-            });
-
-            tableRow.AppendChild(new TableCell(new Paragraph(new Run(new Text("Date:"))
-                {RunProperties = new RunProperties(new Bold())})));
-            tableRow.AppendChild(new TableCell(new Paragraph(new Run(new Text("09/01/2020")))));
-
-            var secondRow = new TableRow();
-            secondRow.AppendChild(new TableCell(new Paragraph(new Run()))
-            {
-                TableCellProperties = new TableCellProperties
-                    {VerticalMerge = new VerticalMerge {Val = MergedCellValues.Continue}}
-            });
-            secondRow.AppendChild(new TableCell(new Paragraph(new Run()))
-            {
-                TableCellProperties = new TableCellProperties
-                    {VerticalMerge = new VerticalMerge {Val = MergedCellValues.Continue}}
-            });
-            secondRow.AppendChild(new TableCell(new Paragraph(new Run(new Text("Author:"))
-                {RunProperties = new RunProperties(new Bold())})));
-            secondRow.AppendChild(new TableCell(new Paragraph(new Run(new Text("Meow Meowington")))));
-
-            var thirdRow = new TableRow();
-            thirdRow.AppendChild(new TableCell(new Paragraph(new Run(new Text("Is AO Required?"))
-                {RunProperties = new RunProperties(new Bold())})));
-            thirdRow.AppendChild(new TableCell(new Paragraph(new Run(new Text("No")))));
-            thirdRow.AppendChild(new TableCell(new Paragraph(new Run(new Text("Cleared By:"))
-                {RunProperties = new RunProperties(new Bold())})));
-            thirdRow.AppendChild(new TableCell(new Paragraph(new Run(new Text("Barks Barkington")))));
-
-            table.AppendChild(tableRow);
-            table.AppendChild(secondRow);
-            table.AppendChild(thirdRow);
-            builder.Body.AppendChild(table);
-
+            builder.AddTable(rows);
+            builder.AddLineBreak();
+            builder.AddTable(rows);
             builder.Build();
             Console.WriteLine("Hello World!");
         }
